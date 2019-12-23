a3bdf5be88f34eba34d3798111ed91a40d05c8d2
sola <opop0628@gmail.com>
2019-12-23 19:02:43 +0900

    tersorboad version prob solve

--- utils/logger.py
+++ utils/logger.py
@@ -4,14 +4,19 @@ import tensorflow as tf
 class Logger(object):
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
-        self.writer = tf.summary.FileWriter(log_dir)
+        #self.writer = tf.summary.FileWriter(log_dir)
+        self.writer = tf.summary.create_file_writer(log_dir)
 
-    def scalar_summary(self, tag, value, step):
+    def scalar_summary(self, name, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        #summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        with self.writer.as_default():
+            tf.summary.scalar(name, value, step=step)    
 
     def list_of_scalars_summary(self, tag_value_pairs, step):
         """Log scalar variables."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
-        self.writer.add_summary(summary, step)
+        #summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
+        #summary = tf.summary.(value=[tf.compat.v1.Summary.Value(tag=tag, simple_value=value) for tag, value in tag_value_pairs])
+        with self.writer.as_default():
+            for name, value in tag_value_pairs : 
+                tf.summary.scalar(name, value, step=step)
